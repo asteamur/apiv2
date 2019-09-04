2abc0c394178e24b75b2bc4f52799ef6a884544c
Unknown <miguelangel.alarcos@asteamur.org>
2019-09-04 13:32:31 +0200

    introduced qs for testing with axios

--- test/test1.js
+++ test/test1.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const { assert } = require('chai')
 const { A, B } = require('../model')
 const axios = require('axios')
+const qs = require('qs')
 
 const uri = 'mongodb://localhost:27017/test'
 let a = null
@@ -29,7 +30,17 @@ after(async ()=>{
 
 describe('suite get', ()=>{
     it('test get a, field a', async ()=>{
-        ret = await axios.get('http://localhost:3000/api/a/' + a._id + '/?fields=a')
+        //ret = await axios.get('http://localhost:3000/api/a/' + a._id + '/?fields=a')
+        ret = await axios.get('http://localhost:3000/api/a/' + a._id, 
+            {
+                params: {
+                    fields: ['a']
+                },
+                paramsSerializer: params => {
+                    return qs.stringify(params)
+                }
+            }
+        )
         assert.deepEqual(ret.data, {
             _id: '' + a._id,
             a: 'yahoo!'
@@ -37,7 +48,17 @@ describe('suite get', ()=>{
     })
 
     it('test get a, field a, b populate b field b1', async ()=>{
-        ret = await axios.get('http://localhost:3000/api/a/' + a._id + '/?fields=a,b,b%2Eb1,b%2Eb2')
+        //ret = await axios.get('http://localhost:3000/api/a/' + a._id + '/?fields=a,b,b%2Eb1,b%2Eb2',
+        ret = await axios.get('http://localhost:3000/api/a/' + a._id,
+            {
+                params: {
+                    fields: ['a', 'b', 'b.b1', 'b.b2']
+                },
+                paramsSerializer: params => {
+                    return qs.stringify(params)
+                }
+            }
+        )
         assert.deepEqual(ret.data, {
             _id: '' + a._id,
             a: 'yahoo!',
@@ -50,21 +71,51 @@ describe('suite get', ()=>{
     })
 
     it('test get several a, field a, c', async ()=>{
-        ret = await axios.get('http://localhost:3000/api/a/?fields=a,c')
+        ret = await axios.get('http://localhost:3000/api/a',
+            {
+                params: {
+                    fields: ['a', 'c']
+                },
+                paramsSerializer: params => {
+                    return qs.stringify(params)
+                }
+            }
+        )
         assert.deepEqual(ret.data.map((x) => {
             return {a: x.a, c: x.c}
         }), [{a: 'yahoo!', c: 0}, {a: 'google!', c: 1}, {a: 'bingo!', c: 2}, {a: 'insert coin', c: 3}])
     })
 
     it('test get several a, regex', async ()=>{
-        ret = await axios.get('http://localhost:3000/api/a/?a=oo&fields=a,c')
+        //ret = await axios.get('http://localhost:3000/api/a/?a=oo&fields=a,c',
+        ret = await axios.get('http://localhost:3000/api/a',
+        {
+            params: {
+                fields: ['a', 'c'],
+                a: 'oo'
+            },
+            paramsSerializer: params => {
+                return qs.stringify(params)
+            }
+        })
         assert.deepEqual(ret.data.map((x) => {
             return {a: x.a, c: x.c}
         }), [{a: 'yahoo!', c: 0}, {a: 'google!', c: 1}])
     })
 
     it('test get several a, field a, c, sort -c, page 1, limit 2', async ()=>{
-        ret = await axios.get('http://localhost:3000/api/a/?page=1&limit=2&sort=-c&fields=a,c')
+        //ret = await axios.get('http://localhost:3000/api/a/?page=1&limit=2&sort=-c&fields=a,c', {
+        ret = await axios.get('http://localhost:3000/api/a', {
+            params: {
+                page: 1,
+                limit: 2,
+                sort: '-c',
+                fields: ['a', 'c']
+            },
+            paramsSerializer: params => {
+                return qs.stringify(params)
+            }
+        })
         assert.deepEqual(ret.data.map((x) => {
             return {a: x.a, c: x.c}
         }), [{a: 'insert coin', c: 3}, {a: 'bingo!', c: 2}])
